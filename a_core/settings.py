60bc1f8698d5c976f34b410841b286624c58aa7a
isaaclikesoreos <isaac.d.mclaurin@icloud.com>
2025-04-05 16:58:01 -0700

    Remove hardcoded credentials from settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -216,8 +216,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # For Gmail
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'isaac.d.mclaurin@gmail.com'
-EMAIL_HOST_PASSWORD = 'pigd btmj oyul jcyp'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 DEFAULT_FROM_EMAIL = 'Magic app <isaac.d.mclaurin@gmail.com>'
 
 
